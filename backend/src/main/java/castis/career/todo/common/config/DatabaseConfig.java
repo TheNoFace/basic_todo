9704fad7b3210cf8c6a9503a56240f9e2f24e0bf
Joonhyuk Song <thenoface303@gmail.com>
2025-05-08 17:00:54 +0900

    feat: Configure Datasource

---
+++ backend/src/main/java/castis/career/todo/common/config/DatabaseConfig.java
@@ -0,0 +1,66 @@
+package castis.career.todo.common.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@EnableJpaRepositories(basePackages = "castis.career.todo")
+public class DatabaseConfig {
+
+    private final HibernateProperties hibernateProperties;
+    private final JpaProperties jpaProperties;
+
+    public DatabaseConfig(HibernateProperties hibernateProperties, JpaProperties jpaProperties) {
+        this.hibernateProperties = hibernateProperties;
+        this.jpaProperties = jpaProperties;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder()
+                .url(dataSourceProperties.getUrl())
+                .username(dataSourceProperties.getUsername())
+                .password(dataSourceProperties.getPassword())
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder, DataSource dataSource) {
+
+        Map<String, Object> properties = hibernateProperties.determineHibernateProperties(
+                jpaProperties.getProperties(), new HibernateSettings());
+
+        return builder.dataSource(dataSource)
+                .packages("castis.career.todo")
+                .properties(properties)
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
