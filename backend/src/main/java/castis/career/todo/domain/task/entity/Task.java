277da5db9948bca04ded559b2efa783a2d29b2c1
Joonhyuk Song <thenoface303@gmail.com>
2025-05-08 17:47:32 +0900

    feat: Todo 엔티티 생성

---
+++ backend/src/main/java/castis/career/todo/domain/task/entity/Task.java
@@ -0,0 +1,32 @@
+package castis.career.todo.domain.task.entity;
+
+import castis.career.todo.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Task extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dateFrom;
+    private LocalDateTime dateTo;
+
+    private String taskTitle;
+    private String taskDescription;
+
+    private Boolean isDone;
+}
